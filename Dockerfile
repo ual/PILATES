402893e6d1bd6c394393ab9a64a5d8c5d9d4e0ce
Max Gardner <gardner.max@gmail.com>
2019-06-27 13:04:53 -0700

    pre container build changes

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,49 @@
 FROM continuumio/miniconda3
 
-WORKDIR /home
+WORKDIR /home/ubuntu
 
-# Python env names and paths
+# env vars
 ENV CONDA_DIR /opt/conda
 ENV PATH $CONDA_DIR/bin:$PATH
-ENV CONDA_ENV_USIM_PY2 usim_py2
-ENV CONDA_ENV_USIM_PY3 usim_py3
+ENV CONDA_ENV_BAUS_ORCA_1_4 BAUS_orca_1_4
+ENV CONDA_ENV_BAUS_ORCA_1_5 BAUS_orca_1_5
 ENV CONDA_ENV_ASYNTH activitysynth
-
-ENV PATH /opt/conda/envs/$CONDA_ENV_USIM_PY2/bin:$PATH
-ENV PATH /opt/conda/envs/$CONDA_ENV_USIM_PY3/bin:$PATH
+ENV PATH /opt/conda/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin:$PATH
+ENV PATH /opt/conda/envs/$CONDA_ENV_BAUS_ORCA_1_5/bin:$PATH
 ENV PATH /opt/conda/envs/$CONDA_ENV_ASYNTH/bin:$PATH
 
-# S3 credentials
-ARG AWS_ACCESS_KEY_ID
-ENV AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-ARG AWS_SECRET_ACCESS_KEY
-ENV AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+ENV PILATES_PATH $WORKDIR/PILATES
+ENV LOG_PATH $PILATES_PATH/logs
 
-# Runtime args
-ARG IN_YEAR
-ARG OUT_YEAR
+ENV BAUS_PATH $WORKDIR/bayarea_urbansim
+ENV BAUS_DATA_STORE_PATH $BAUS_PATH/data
+ENV BAUS_DATA_OUTPUT_PATH $BAUS_PATH/output
+ENV BAUS_DATA_OUTPUT_FILE model_data_output.h5
+ENV BAUS_DATA_OUTPUT_FILEPATH $BAUS_DATA_OUTPUT_PATH/$BAUS_DATA_OUTPUT_FILE
+
+ARG BAUS_INPUT_BUCKET=urbansim-inputs
+ENV BAUS_INPUT_BUCKET $BAUS_INPUT_BUCKET
+ENV BAUS_INPUT_BUCKET_PATH s3://$BAUS_INPUT_BUCKET
+
+ARG BAUS_OUTPUT_BUCKET=urbansim-outputs
+ENV BAUS_OUTPUT_BUCKET $BAUS_OUTPUT_BUCKET
+ENV BAUS_OUTPUT_BUCKET_PATH s3://$BAUS_OUTPUT_BUCKET
+
+ENV ASYNTH_PATH $WORKDIR/activitysynth
+ENV ASYNTH_DATA_PATH $ASYNTH_PATH/activitysynth/data
+ENV ASYNTH_DATA_OUTPUT_PATH $ASYNTH_PATH/activitysynth/output
+ENV ASYNTH_DATA_OUTPUT_FILE model_data_output.h5
+ENV ASYNTH_DATA_OUTPUT_FILEPATH $ASYNTH_DATA_OUTPUT_PATH/$ASYNTH_DATA_OUTPUT_FILE
+
+ARG SKIMS_BUCKET=urbansim-beam
+ENV SKIMS_BUCKET $SKIMS_BUCKET
 
 
 # Build Python envs
 RUN conda update conda
 
 # BAUS Estimation Python Environment
-RUN conda create --quiet --yes --channel conda-forge -p $CONDA_DIR/envs/$CONDA_ENV_USIM_PY2 \
+RUN conda create --quiet --yes --channel conda-forge -p $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4 \
 	python=2.7 \
 	numpy=1.10.0 \
 	scipy \
@@ -40,13 +55,13 @@ RUN conda create --quiet --yes --channel conda-forge -p $CONDA_DIR/envs/$CONDA_E
 	boto
 
 RUN git clone https://github.com/ual/bayarea_urbansim.git \
-	&& cd bayarea_urbansim \
-	&& $CONDA_DIR/envs/$CONDA_ENV_USIM_PY2/bin/python -m pip install -r requirements.txt
+	&& cd $BAUS_PATH \
+	&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python -m pip install -r requirements.txt
 
 
 # BAUS Simulation Python Environment
-RUN conda create --quiet --yes -p $CONDA_DIR/envs/$CONDA_ENV_USIM_PY3 --clone $CONDA_ENV_USIM_PY2
-RUN conda install -p $CONDA_DIR/envs/$CONDA_ENV_USIM_PY3 -c udst orca
+RUN conda create --quiet --yes -p $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_5 --clone $CONDA_ENV_BAUS_ORCA_1_4
+RUN conda install -p $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_5 -c udst orca
 
 
 # ActivitySynth Python Environment
@@ -59,16 +74,17 @@ RUN conda create --quiet --yes --channel conda-forge -p $CONDA_DIR/envs/$CONDA_E
 	s3fs \
 	scipy < 1.3
 RUN git clone https://github.com/ual/activitysynth.git \
-	&& cd activitysynth \
+	&& cd $ASYNTH_PATH \
 	&& $CONDA_DIR/envs/$CONDA_ENV_ASYNTH/bin/python setup.py install
 RUN conda config --add channels udst
 RUN conda config --add channels conda-forge
 RUN conda install --quiet --yes -p $CONDA_DIR/envs/$CONDA_ENV_ASYNTH -c udst pandana
 
 
-# Make model data .h5
-RUN $CONDA_DIR/envs/$CONDA_ENV_USIM_PY2/bin/python make_model_data_hdf.py -y base
+# Get PILATES repo from github
+RUN git clone https://github.com/ual/PILATES.git
 
-
-# Run data pre-processing step
-RUN $CONDA_DIR/envs/$CONDA_ENV_USIM_PY2/bin/python baus.py --mode preprocessing
+# Run PILATES and accept run-time arguments
+WORKDIR /home/ubuntu/PILATES
+ENTRYPOINT ["/bin/bash", "-c", "pilates.sh"]
+CMD []
---
+++ pilates.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+# define default values for env vars if not set
+export CONDA_DIR=${CONDA_DIR:-~/anaconda3}
+export CONDA_ENV_BAUS_ORCA_1_4=${CONDA_ENV_BAUS_ORCA_1_4:-baus}
+export CONDA_ENV_BAUS_ORCA_1_5=${CONDA_ENV_BAUS_ORCA_1_5:-baus_output}
+export CONDA_ENV_ASYNTH=${CONDA_ENV_ASYNTH:-activitysynth}
+
+export PILATES_PATH=${PILATES_PATH:-~/projects/PILATES}
+export LOG_PATH=$PILATES_PATH/logs
+
+export BAUS_PATH=${BAUS_PATH:-~/projects/bayarea_urbansim}
+export BAUS_DATA_STORE_PATH=$BAUS_PATH/data
+export BAUS_DATA_OUTPUT_PATH=$BAUS_PATH/output
+export BAUS_DATA_OUTPUT_FILE=${BAUS_DATA_OUTPUT_FILE:-model_data_output.h5}
+export BAUS_DATA_OUTPUT_FILEPATH=$BAUS_DATA_OUTPUT_PATH/$BAUS_DATA_OUTPUT_FILE
+export BAUS_INPUT_BUCKET=${BAUS_INPUT_BUCKET:-urbansim-inputs}
+export BAUS_INPUT_BUCKET_PATH=s3://$BAUS_INPUT_BUCKET
+export BAUS_OUTPUT_BUCKET=${BAU_OUTPUT_BUCKET:-urbansim-outputs}
+export BAUS_OUTPUT_BUCKET_PATH=s3://$BAUS_OUTPUT_BUCKET
+
+
+export SKIMS_BUCKET=${SKIMS_BUCKET:-urbansim-beam}
+
+export ASYNTH_PATH=${ASYNTH_PATH:-~/projects/activitysynth}
+export ASYNTH_DATA_PATH=$ASYNTH_PATH/activitysynth/data
+export ASYNTH_DATA_OUTPUT_PATH=$ASYNTH_PATH/activitysynth/output
+export ASYNTH_DATA_OUTPUT_FILE=${ASYNTH_DATA_OUTPUT_FILE:-model_data_output.h5}
+export ASYNTH_DATA_OUTPUT_FILEPATH="$ASYNTH_DATA_OUTPUT_PATH/$ASYNTH_DATA_OUTPUT_FILE"
+
+# define default values/behavior for command-line arguments
+export IN_YEAR=${1:?required run-time argument \#1 not passed in}
+export OUT_YEAR=${2:?required run-time argument \#2 not passed in}
+export BAUS_ITER_FREQ=${3:?required run-time argument \#3 not passed in}
+export SCENARIO=${4:?required run-time argument \#4 not passed in}
+export SKIMS_FNAME=${5:?required run-time argument \#5 not passed in}
+export IN_YEAR_OUTPUT=${6:-off}
+
+export SKIMS_FILEPATH=s3://$SKIMS_BUCKET/$SKIMS_FNAME
+
+
+# send stdout and stderr to console and logs
+exec > >(tee $LOG_PATH/$(date +%d%B%Y_%H%M%S).log)
+exec 2>&1
+
+
+# 2010 data can be base input data as well so we call it
+# base data when its base data even though the year is 2010
+if [[ $IN_YEAR == 2010 ]]; then
+	export BAUS_INPUT_DATA_YEAR="base"
+else
+	export BAUS_INPUT_DATA_YEAR=$IN_YEAR
+fi
+
+
+# Make in-year model data .h5
+cd $PILATES_PATH/scripts \
+&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python \
+make_model_data_hdf.py -n -b \
+-i $BAUS_INPUT_BUCKET_PATH/$SCENARIO/$BAUS_INPUT_DATA_YEAR \
+-s $SKIMS_FILEPATH -o $BAUS_DATA_STORE_PATH
+
+
+# Run data pre-processing step
+cd $BAUS_PATH \
+&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python baus.py -c \
+--mode preprocessing
+
+
+# Run bayarea_urbansim model estimation
+cd $BAUS_PATH \
+&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python baus.py -c \
+--mode estimation
+
+
+# Run bayarea_urbansim simulation 
+cd $BAUS_PATH \
+&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_5/bin/python baus.py -c -o \
+-y $IN_YEAR,$OUT_YEAR -n $BAUS_ITER_FREQ --mode simulation
+
+
+# generate in-year ouputs if specified
+if [[ $IN_YEAR_OUTPUT == "on" ]]; then
+
+	# Write base year baus outputs to csv
+	cd $PILATES_PATH/scripts \
+	&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python \
+	make_csvs_from_output_store.py -y $IN_YEAR -d $BAUS_DATA_OUTPUT_FILEPATH \
+	-o $ASYNTH_DATA_PATH
+
+	# Run activitysynth for base year
+	cd $ASYNTH_PATH/activitysynth \
+	&& $CONDA_DIR/envs/$CONDA_ENV_ASYNTH/bin/python run.py -o
+
+	# Write in-year activitysynth outputs to s3 as .csv and then 
+	# delete the output data from the default output data directory
+	# so that orca doesn't just append to it next time around
+	cd $PILATES_PATH/scripts \
+	&& $CONDA_DIR/envs/$CONDA_ENV_ASYNTH/bin/python \
+	make_csvs_from_output_store.py -d $ASYNTH_DATA_OUTPUT_FILEPATH \
+	-o $BAUS_OUTPUT_BUCKET_PATH/$(date +%d%B%Y)/$SCENARIO/$IN_YEAR -x
+
+	echo "Wrote ActivitySynth outputs for "
+fi
+
+
+# Write end year baus outputs to csv
+cd $PILATES_PATH/scripts \
+&& $CONDA_DIR/envs/$CONDA_ENV_BAUS_ORCA_1_4/bin/python \
+make_csvs_from_output_store.py -y $OUT_YEAR -d $BAUS_DATA_OUTPUT_FILEPATH \
+-o $ASYNTH_DATA_PATH
+
+
+# Run activitysynth for end-year
+cd $ASYNTH_PATH/activitysynth \
+&& $CONDA_DIR/envs/$CONDA_ENV_ASYNTH/bin/python run.py -o
+
+
+# Write out-year activitysynth outputs to s3 as .csv
+cd $PILATES_PATH/scripts && $CONDA_DIR/envs/$CONDA_ENV_ASYNTH/bin/python \
+make_csvs_from_output_store.py -d $ASYNTH_DATA_OUTPUT_FILEPATH \
+-o $BAUS_OUTPUT_BUCKET_PATH/$(date +%d%B%Y)/$SCENARIO/$OUT_YEAR
+
+
