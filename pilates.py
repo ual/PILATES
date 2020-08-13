e31f192097fb7fa3fb8c7966eb0df9f81133b7b8
mxndrwgrdnr <magardner@berkeley.edu>
2020-08-13 23:45:27 +0000

    fixed asim indexing bug, pilates now works reading usim output into asim.

--- pilates.py
+++ pilates.py
@@ -3,7 +3,6 @@ import docker
 import os
 import s3fs
 
-
 region_to_asim_subdir = {
     'austin': 'austin_mp',
     'detroit': 'detroit',
@@ -58,7 +57,8 @@ if __name__ == '__main__':
 
     # make sure user that executes pilates.py can run docker without sudo
     client = docker.from_env()
-    for year in range(start_year, end_year, travel_model_freq):
+    # for year in range(start_year, end_year, travel_model_freq):
+    for year in [start_year]:
 
         forecast_year = year + travel_model_freq
         print(
@@ -68,36 +68,47 @@ if __name__ == '__main__':
         # run urbansim
         # TO DO: FIX STDOUT PRINTING SO IT DOESN'T LOOK LIKE GARBAGE
         # TO DO: FIX PYPROJ ERRORS (prob use "conda run -n" as entrypoint)
-        # usim = client.containers.run(
-        #     land_use_image,
-        #     command="-i {0} -o {1} -v {2} -b {3} --scenario {4} -u {5}".format(
-        #         year, forecast_year,
-        #         land_use_freq, usim_bucket, scenario, beam_skims_url),
-        #     stderr=True, detach=True, remove=True)
-        # for log in usim.logs(stream=True, stderr=True, stdout=True):
-        #     print(log)
+
+        usim = client.containers.run(
+            land_use_image,
+            command="-i {0} -o {1} -v {2} -b {3} --scenario {4} -u {5}".format(
+                year, forecast_year,
+                land_use_freq, usim_bucket, scenario, beam_skims_url),
+            stderr=True, detach=True, remove=True)
+        for log in usim.logs(stream=True, stderr=True, stdout=True):
+            print(log)
 
         # copy urbansim outputs to activitysim inputs
-        print('Copying {0} {1} outputs to {2} input directory'.format(
-            forecast_year, land_use_image, activity_demand_image))
         formattable_data_path = os.path.join(
             '{0}', '{1}', '{2}', '{3}', 'model_data.h5')
         usim_data_path = formattable_data_path.format(
             usim_bucket, 'output', scenario, forecast_year)
         asim_data_path = formattable_data_path.format(
             asim_bucket, 'input', scenario, forecast_year)
+
+        print('Copying data from {0} to {1} input directory'.format(
+            asim_data_path, usim_data_path))
+
         s3fs.S3FileSystem.read_timeout = 84600
         s3 = s3fs.S3FileSystem(config_kwargs={'read_timeout': 86400})
         s3.cp(usim_data_path, asim_data_path)
 
         # run activitysim
-        print("Generating activity plans for the year {0}".format(
-            forecast_year))
+        print("Generating activity plans for the year {0} with {1}".format(
+            forecast_year, activity_demand_image))
+
+        # only copy results to urbansim inputs if not base year. usually
+        # only the case for warm start or debugging.
+        if forecast_year == start_year:
+            formattable_asim_cmd_str = '-y {0} -s {1} -b {2} -u {3}'
+        else:
+            formattable_asim_cmd_str = '-y {0} -s {1} -b {2} -u {3} -w'
+
         asim = client.containers.run(
             activity_demand_image, working_dir=asim_workdir,
-            command='-y {0} -s {1} -b {2} -u {3}'.format(
+            command=formattable_asim_cmd_str.format(
                 forecast_year, scenario, asim_bucket, beam_skims_url),
-            stderr=True, detach=True, remove=True)
+            stdout=True, stderr=True, detach=True, remove=True)
         for log in asim.logs(stream=True, stderr=True, stdout=True):
             print(log)
 
