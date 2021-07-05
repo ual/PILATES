63da3fd02d984ba5b0c3b6c9bd469802c345008c
Dmitry Openkov <dimaopen@gmail.com>
2021-07-05 10:08:08 +0300

    Added activitysim_beam_run.py which executes ASim and Beam

--- pilates/activitysim/postprocessor.py
+++ pilates/activitysim/postprocessor.py
@@ -211,6 +211,19 @@ def create_next_iter_inputs(settings, year):
     return
 
 
+def copy_output_plans_to_beam(settings):
+    asim_output_data_dir = settings['asim_local_output_folder']
+    plans_path = os.path.join(asim_output_data_dir, 'final_plans.csv')
+    beam_local_plans = os.path.join(settings['beam_local_input_folder'], settings['beam_plans'])
+
+    logger.info("Copying asim plans %s to beam input plans %s", plans_path, beam_local_plans)
+    import gzip
+    with open(plans_path, 'rb') as f_in, gzip.open(beam_local_plans, 'wb') as f_out:
+        f_out.writelines(f_in)
+
+    return
+
+
 # def cleanup():
 #     """
 #     Method to remove/archive data from the input directory that was
