fb9288275e4abac7080e6e3261d43b4f34c59ddc
Max Gardner <gardner.max@gmail.com>
2021-07-16 19:38:43 -0700

    austin usim model now runs on polaris skims

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import sys
 from pilates.activitysim import preprocessor as asim_pre
 from pilates.activitysim import postprocessor as asim_post
 from pilates.urbansim import preprocessor as usim_pre
+from pilates.urbansim import postprocessor as usim_post
 
 logging.basicConfig(
     stream=sys.stdout, level=logging.INFO,
@@ -29,35 +30,23 @@ if __name__ == '__main__':
     with open('settings.yaml') as file:
         settings = yaml.load(file, Loader=yaml.FullLoader)
 
-    s3_io = settings['s3_io']
-    land_use_image = settings['land_use_image']
-    activity_demand_image = settings['activity_demand_image']
-    travel_model_image = settings['travel_model_image']
+    # scenario settings
+    image_names = settings['docker_images']
+    land_use_model = settings.get('land_use_model', False)
+    activity_demand_model = settings.get('activity_demand_model', False)
+    travel_model = settings.get('travel_model', False)
     region = settings['region']
+    region_id = settings['region_to_region_id'][region]
     scenario = settings['scenario']
     start_year = settings['start_year']
     end_year = settings['end_year']
-    land_use_freq = settings['land_use_freq']
     travel_model_freq = settings['travel_model_freq']
-    household_sample_size = settings['household_sample_size']
     path_to_skims = settings['path_to_skims']
-    beam_local_config = settings['beam_local_config']
-    beam_local_input_folder = settings['beam_local_input_folder']
-    beam_local_output_folder = settings['beam_local_output_folder']
-    skim_zone_source_id_col = settings['skim_zone_source_id_col']
-    usim_client_data_folder = settings['usim_client_data_folder']
-    usim_local_data_folder = settings['usim_local_data_folder']
-    asim_bucket = settings['region_to_asim_bucket'][region]
-    asim_subdir = settings['region_to_asim_subdir'][region]
-    asim_workdir = os.path.join('/activitysim', asim_subdir)
-    chunk_size = settings['chunk_size']
-    num_processes = settings['num_processes']
-    asim_local_input_folder = settings['asim_local_input_folder']
-    asim_local_output_folder = settings['asim_local_output_folder']
-    beam_subdir = settings['region_to_beam_subdir'][region]
-    docker_stdout = settings['docker_stdout']
-    pull_latest = settings['pull_latest']
-    region_id = settings['region_to_region_id'][region]
+
+    # PILATES defaults
+    household_sample_size = settings.get('household_sample_size', 0)
+    docker_stdout = settings.get('docker_stdout', False)
+    pull_latest = settings.get('pull_latest', False)
 
     # parse args
     parser = argparse.ArgumentParser(add_help=False)
@@ -80,9 +69,11 @@ if __name__ == '__main__':
     # prep docker environment
     client = docker.from_env()
     if pull_latest:
-        for image in [
-                land_use_image, activity_demand_image, travel_model_image]:
-            client.images.pull(image)
+        for model in [land_use_model, activity_demand_model, travel_model]:
+            if model:
+                image = image_names[model]
+                print('Pulling latest image for {0}'.format(image))
+                client.images.pull(image)
 
     # formattable runtime docker command strings
     formattable_usim_cmd = '-r {0} -i {1} -y {2} -f {3}'
@@ -90,11 +81,20 @@ if __name__ == '__main__':
     # run the simulation flow
     for year in range(start_year, end_year, travel_model_freq):
 
-        if land_use_freq > 0:
+        if land_use_model:
 
-            forecast_year = year + travel_model_freq
+            ###########################
+            #    FORECAST LAND USE    #
+            ###########################
 
-            # 0. PREPARE URBANSIM DATA
+            # land use settings
+            land_use_image = image_names[land_use_model]
+            land_use_freq = settings['land_use_freq']
+            skim_zone_source_id_col = settings['skim_zone_source_id_col']
+            usim_client_data_folder = settings['usim_client_data_folder']
+            usim_local_data_folder = settings['usim_local_data_folder']
+
+            # 1. PREPARE URBANSIM DATA
             print_str = (
                 "Preparing input data for land use development simulation.")
             formatted_print(print_str)
@@ -102,7 +102,8 @@ if __name__ == '__main__':
             usim_pre.add_skims_to_model_data(
                 settings, region, skim_zone_source_id_col)
 
-            # 1. RUN URBANSIM
+            # 2. RUN URBANSIM
+            forecast_year = year + travel_model_freq
             print_str = (
                 "Simulating land use development from {0} "
                 "to {1} with {2}.".format(
@@ -125,69 +126,101 @@ if __name__ == '__main__':
         else:
             forecast_year = year
 
-        # 3. PREPROCESS DATA FOR ACTIVITYSIM
-        print_str = "Creating {0} input data from {1} outputs".format(
-            activity_demand_image.split('/')[1], land_use_image.split('/')[1])
-        formatted_print(print_str)
-        asim_pre.create_skims_from_beam(asim_local_input_folder, settings)
-        asim_pre.create_asim_data_from_h5(settings, forecast_year)
+        if activity_demand_model:
 
-        # 4. RUN ACTIVITYSIM
-        print_str = (
-            "Generating activity plans for the year "
-            "{0} with {1}".format(
-                forecast_year, activity_demand_image))
-        formatted_print(print_str)
-        formattable_asim_cmd = '-y {0} -h {1} -n {2} -c {3}'
-        asim = client.containers.run(
-            activity_demand_image, working_dir=asim_workdir,
-            volumes={
-                os.path.abspath(settings['asim_local_input_folder']): {
-                    'bind': os.path.join(asim_workdir, 'data'),
-                    'mode': 'rw'},
-                os.path.abspath(settings['asim_local_output_folder']): {
-                    'bind': os.path.join(asim_workdir, 'output'),
-                    'mode': 'rw'}
-            },
-            command=formattable_asim_cmd.format(
-                forecast_year, household_sample_size,
-                num_processes, chunk_size
-            ),
-            stdout=docker_stdout, stderr=True, detach=True, remove=True)
-        for log in asim.logs(stream=True, stderr=True, stdout=docker_stdout):
-            print(log)
+            #################################
+            #    GENERATE ACTIVITY PLANS    #
+            #################################
+            activity_demand_image = image_names[activity_demand_model]
 
-        # 5. COPY ACTIVITYSIM OUTPUT --> URBANSIM INPUT
-        print_str = (
-            "Generating {0} BEAM and UrbanSim input data from "
-            "{1} outputs".format(
-                forecast_year, activity_demand_image))
-        formatted_print(print_str)
+            asim_subdir = settings['region_to_asim_subdir'][region]
+            asim_workdir = os.path.join('/activitysim', asim_subdir)
+            chunk_size = settings['chunk_size']
+            num_processes = settings['num_processes']
+            asim_local_input_folder = settings['asim_local_input_folder']
+            asim_local_output_folder = settings['asim_local_output_folder']
 
-        # If generating activities for the base year, don't overwrite
-        # urbansim input data. This is usually only the case for warm
-        # starts or debugging. Otherwise we want to set up urbansim for
-        # the next simulation iteration
-        if forecast_year != start_year:
-            asim_post.create_next_iter_inputs(settings, forecast_year)
+            # 1. PREPROCESS DATA FOR ACTIVITY DEMAND MODEL
+            print_str = "Creating {0} input data from {1} outputs".format(
+                activity_demand_image.split('/')[1],
+                land_use_image.split('/')[1])
+            formatted_print(print_str)
+            asim_pre.create_skims_from_beam(asim_local_input_folder, settings)
+            asim_pre.create_asim_data_from_h5(settings, forecast_year)
 
-        # # 6. RUN BEAM
-        # path_to_beam_config = os.path.join(
-        #     beam_local_input_folder, "input", beam_subdir,
-        #     beam_local_config)
-        # client.containers.run(
-        #     travel_model_image,
-        #     volumes={
-        #         beam_local_input_folder: {
-        #             'bind': '/app/{0}'.format(beam_local_input_folder),
-        #             'mode': 'rw'},
-        #         beam_local_output_folder: {
-        #             'bind': '/app/output',
-        #             'mode': 'rw'}},
-        #     command="--config={0}".format(path_to_beam_config),
-        #     stdout=docker_stdout, stderr=True, detach=True, remove=True
-        # )
+            # 2. GENERATE ACTIVITY PLANS
+            print_str = (
+                "Generating activity plans for the year "
+                "{0} with {1}".format(
+                    forecast_year, activity_demand_image))
+            formatted_print(print_str)
+            formattable_asim_cmd = '-y {0} -h {1} -n {2} -c {3}'
+            asim = client.containers.run(
+                activity_demand_image, working_dir=asim_workdir,
+                volumes={
+                    os.path.abspath(settings['asim_local_input_folder']): {
+                        'bind': os.path.join(asim_workdir, 'data'),
+                        'mode': 'rw'},
+                    os.path.abspath(settings['asim_local_output_folder']): {
+                        'bind': os.path.join(asim_workdir, 'output'),
+                        'mode': 'rw'}
+                },
+                command=formattable_asim_cmd.format(
+                    forecast_year, household_sample_size,
+                    num_processes, chunk_size
+                ),
+                stdout=docker_stdout, stderr=True, detach=True, remove=True)
+            for log in asim.logs(
+                    stream=True, stderr=True, stdout=docker_stdout):
+                print(log)
 
-        # # # update path to skims
-        # # new_skims_path = ????
-        # # settings['path_to_skims'] = new_skims_path
+            # 3. COPY ACTIVITY DEMAND OUTPUTS --> LAND USE INPUTS
+            print_str = (
+                "Generating {0} BEAM and UrbanSim input data from "
+                "{1} outputs".format(
+                    forecast_year, activity_demand_image))
+            formatted_print(print_str)
+
+            # If generating activities for the base year, don't overwrite
+            # urbansim input data. This is usually only the case for warm
+            # starts or debugging. Otherwise we want to set up urbansim for
+            # the next simulation iteration
+            if forecast_year != start_year:
+                asim_post.create_next_iter_inputs(settings, forecast_year)
+
+        else:
+
+            # CONVERT LAND USE OUTPUTS TO NEXT ITERATION
+            usim_post.create_next_iter_usim_data(settings, year)
+
+        if travel_model:
+            continue
+            #################################
+            #    RUN TRAFFIC ASSIGNMENT    #
+            #################################
+            # travel_model_image = image_names[travel_model]
+            # beam_local_config = settings['beam_local_config']
+            # beam_local_input_folder = settings['beam_local_input_folder']
+            # beam_local_output_folder = settings['beam_local_output_folder']
+            # beam_subdir = settings['region_to_beam_subdir'][region]
+
+            # # 6. RUN BEAM
+            # path_to_beam_config = os.path.join(
+            #     beam_local_input_folder, "input", beam_subdir,
+            #     beam_local_config)
+            # client.containers.run(
+            #     travel_model_image,
+            #     volumes={
+            #         beam_local_input_folder: {
+            #             'bind': '/app/{0}'.format(beam_local_input_folder),
+            #             'mode': 'rw'},
+            #         beam_local_output_folder: {
+            #             'bind': '/app/output',
+            #             'mode': 'rw'}},
+            #     command="--config={0}".format(path_to_beam_config),
+            #     stdout=docker_stdout, stderr=True, detach=True, remove=True
+            # )
+
+            # # # update path to skims
+            # # new_skims_path = ????
+            # # settings['path_to_skims'] = new_skims_path
