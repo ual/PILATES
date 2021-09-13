f96a76f4c272b4fbb9cd98b5b22bdcccdfce52e2
Randy Weimer <randy.weimer@gmail.com>
2021-09-13 17:19:21 -0500

    SMall config changes after merge

--- clean-data.sh
+++ clean-data.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-echo $PWD
+BACKUP_DIR=/mnt/c/Users/rweimer/polaris/UrbanSim_files
+SCENARIO_DIR=/mnt/c/Users/rweimer/polaris/UrbanSim_files
+SOURCE_DIR=/mnt/c/Users/rweimer/polaris/campoInputs-orig
+MODEL_DIR=/mnt/c/Users/rweimer/polaris/campoInputs
+echo current directory: $PWD
+echo scenario directory: $SCENARIO_DIR
+echo source directory: $SOURCE_DIR
+echo model directory: $MODEL_DIR
 
 rm ./pilates/polaris/data/*
 rm ./pilates/urbansim/data/*
 
-cp ./pilates/backup/campo_skims.hdf5 pilates/polaris/data/
-cp ./pilates/backup/custom_mpo_*.h5 pilates/urbansim/data/
+cp $BACKUP_DIR/campo_skims.hdf5 pilates/polaris/data/
+cp $BACKUP_DIR/custom_mpo_*.h5 pilates/urbansim/data/
 
-cp /mnt/e/polaris/UrbanSim_files/*.json /mnt/e/polaris/Repos/campoInputs
-cp /mnt/e/polaris/UrbanSim_files/*.csv /mnt/e/polaris/Repos/campoInputs
-cp /mnt/e/polaris/campoInputs-orig/*.sqlite /mnt/e/polaris/Repos/campoInputs
-cp /mnt/e/polaris/campoInputs-orig/*.bin /mnt/e/polaris/Repos/campoInputs
+cp $SCENARIO_DIR/*.json   $MODEL_DIR
+cp $SCENARIO_DIR/*.csv    $MODEL_DIR
+cp $SOURCE_DIR/*.sqlite $MODEL_DIR
+cp $SOURCE_DIR/*.bin $MODEL_DIR
