d74438b3eff0fb05496856e48c5d292147023d7f
Dmitry Openkov <dimaopen@gmail.com>
2021-11-05 11:31:07 +0300

    Removed multiple copying of asim data to beam input folder

--- pilates/beam/preprocessor.py
+++ pilates/beam/preprocessor.py
@@ -6,7 +6,7 @@ import shutil
 logger = logging.getLogger(__name__)
 
 
-def copy_plans_from_asim(settings, year, replanning_iteration=-1):
+def copy_plans_from_asim(settings, year, replanning_iteration_number=0):
     asim_output_data_dir = settings['asim_local_output_folder']
     beam_scenario_folder = os.path.join(
         settings['beam_local_input_folder'],
@@ -23,13 +23,12 @@ def copy_plans_from_asim(settings, year, replanning_iteration=-1):
             f_out.writelines(f_in)
 
     copy_with_compression_asim_file_to_beam('final_plans.csv', 'plans.csv.gz')
-    if replanning_iteration == -1:
+    if replanning_iteration_number == 0:
         copy_with_compression_asim_file_to_beam('final_households.csv', 'households.csv.gz')
         copy_with_compression_asim_file_to_beam('final_persons.csv', 'persons.csv.gz')
 
     if settings.get('final_asim_plans_folder', False):
         beam_local_plans = os.path.join(beam_scenario_folder, 'plans.csv.gz')
-        replanning_iteration_number = replanning_iteration + 1
         final_plans_name = f"final_plans_{year}_{replanning_iteration_number:02d}.csv.gz"
         final_plans_location = os.path.join(settings['final_asim_plans_folder'], final_plans_name)
         logger.info("Copying asim plans %s to final asim folder %s", beam_local_plans, final_plans_location)
