9c96fcf19a32b95217301fdfb1a397ac45577918
yhanw0719 <62628137+yhanw0719@users.noreply.github.com>
2022-01-25 14:10:37 -0800

    Update preprocessor.py
    add a warm_start parameter, so this preprocess can be called in warm_start too.

--- pilates/atlas/preprocessor.py
+++ pilates/atlas/preprocessor.py
@@ -8,21 +8,23 @@ import logging
 
 # import yaml
 # import argparse
-# def parse_args_and_settings(settings_file='settings.yaml'):
-
-#     # read settings from config file
-#     with open(settings_file) as file:
-#         settings = yaml.load(file, Loader=yaml.FullLoader)
-#     return settings
-# settings = parse_args_and_settings()
-
+# with open('settings.yaml') as file:
+#     settings = yaml.load(file, Loader=yaml.FullLoader)
 
 logger = logging.getLogger(__name__)
 
-def prepare_atlas_inputs(settings, year):
+
+def prepare_atlas_inputs(settings, year, warm_start=False):
     # set where to find urbansim output 
     urbansim_output_path = settings['usim_local_data_folder']
-    urbansim_output_fname = 'model_data_{}.h5'.format(year)
+    if warm_start:
+        # if warm start, read custom_mpo h5
+        region = settings['region']
+        region_id = settings['region_to_region_id'][region]
+        urbansim_output_fname = 'custom_mpo_{}_model_data.h5'.format(region_id)
+    else:
+        # if in main loop, read urbansim-generated h5 
+        urbansim_output_fname = 'model_data_{}.h5'.format(year)
     urbansim_output = os.path.join(urbansim_output_path,urbansim_output_fname)
     
     # set where to put atlas csv inputs (processed from urbansim outputs)
@@ -35,27 +37,30 @@ def prepare_atlas_inputs(settings, year):
     
     # read urbansim h5 outputs
     with pd.HDFStore(urbansim_output,mode='r') as data:
+        if not warm_start:
+            data = data['/{}'.format(year)]
+
         try:
             # prepare households atlas input
-            households = data['{}/households'.format(year)]
+            households = data['/households']
             households.to_csv('{}/households.csv'.format(atlas_input_path))
 
             # prepare blocks atlas input
-            blocks = data['{}/blocks'.format(year)]
+            blocks = data['/blocks']
             blocks.to_csv('{}/blocks.csv'.format(atlas_input_path))          
 
             # prepare persons atlas input
-            persons = data['{}/persons'.format(year)]
+            persons = data['/persons']
             persons.to_csv('{}/persons.csv'.format(atlas_input_path))
 
             # prepare residential unit atlas input
-            residential_units = data['{}/residential_units'.format(year)]
+            residential_units = data['/residential_units']
             residential_units.to_csv('{}/residential.csv'.format(atlas_input_path))
 
             # prepare jobs atlas input
-            jobs = data['{}/jobs'.format(year)]
+            jobs = data['/jobs']
             jobs.to_csv('{}/jobs.csv'.format(atlas_input_path))
 
         except: 
-            logger.info('Urbansim Year {} Output Was Not Loaded Correctly by ATLAS')
+            logger.error('Urbansim Year {} Output Was Not Loaded Correctly by ATLAS')
 
