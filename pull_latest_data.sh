e8580db5696c754aadc749bdcf57e3fff233789c
Zach Needell <zaneedell@lbl.gov>
2022-02-04 14:53:54 -0800

    script to pull beam data

---
+++ pull_latest_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+reset=$(tput sgr0)
+
+for i in $(find pilates/beam/production -maxdepth 1 -mindepth 1 -type d); do
+    if [ -d $i/.git ]; then
+        dirname=$(basename "$i")
+        repo="pilates/beam/production/$dirname/.git"
+        worktree="$PWD/pilates/beam/production/$dirname"
+        echo "----------"
+        echo "${blue}$dirname${reset}"
+        echo "${yellow}Step 1 of 2: checking out to pilates data branch...${reset}"
+        git --git-dir=$repo --work-tree=$worktree checkout pilates -f
+        git --git-dir=$repo --work-tree=$worktree reset --hard HEAD
+        echo "${yellow}Step 2 of 2: pulling from pilates...${reset}"
+        git --git-dir=$repo --work-tree=$worktree pull
+    fi
+done
