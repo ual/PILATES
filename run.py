ceca603d88f5ac945b63aea43d9a6e5988683218
yhanw0719 <62628137+yhanw0719@users.noreply.github.com>
2022-02-10 23:31:00 -0800

    incorporate ual-clean latest update in run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from re import L
+import shutil
 import yaml
 import docker
 import os
@@ -37,6 +38,21 @@ def find_latest_beam_iteration(beam_output_dir):
             if dir == "ITER":
                 iter_dirs += os.path.join(root, dir)
     print(iter_dirs)
+
+def setup_beam_skims(settings):
+    region = settings['region']
+    beam_input_dir = settings['beam_local_input_folder']
+    beam_output_dir = settings['beam_local_output_folder']
+    skims_fname = settings['skims_fname']
+    input_skims_location = os.path.join(beam_input_dir, region, skims_fname)
+    mutable_skims_location = os.path.join(beam_output_dir, skims_fname)
+
+    logger.info("Copying input skims from {0} to {1}".format(
+        input_skims_location,
+        mutable_skims_location))
+
+    shutil.copyfile(input_skims_location, mutable_skims_location)
+
 
 def parse_args_and_settings(settings_file='settings.yaml'):
 
@@ -423,7 +439,8 @@ def generate_activity_plans(
         activity_demand_model,
         land_use_model)
     formatted_print(print_str)
-    asim_pre.create_skims_from_beam(settings, year, overwrite=overwrite_skims)
+    asim_pre.create_skims_from_beam(
+        settings, year=forecast_year, overwrite=overwrite_skims)
     asim_pre.create_asim_data_from_h5(
         settings, year=forecast_year, warm_start=warm_start)
 
@@ -615,7 +632,7 @@ def run_replanning_loop(settings, forecast_year):
         formatted_print(print_str)
 
         # a) format new skims for asim
-        asim_pre.create_skims_from_beam(settings, year, overwrite=True)
+        asim_pre.create_skims_from_beam(settings, forecast_year, overwrite=True)
 
         # b) replan with asim
         print_str = (
@@ -673,6 +690,9 @@ if __name__ == '__main__':
     elif static_skims:
         formatted_print('"STATIC SKIMS" MODE ENABLED')
         logger.info('Using the same set of skims for every iteration.')
+
+    if settings.get('travel_model') == 'beam':
+        setup_beam_skims(settings)
 
     # start docker client
     client = initialize_docker_client(settings)
