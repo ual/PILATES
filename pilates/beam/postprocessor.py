6a5cdbe1cd569be5477651abb45e70adfbb2d71c
Zach Needell <zaneedell@lbl.gov>
2022-02-24 21:02:24 -0800

    dumb bug

--- pilates/beam/postprocessor.py
+++ pilates/beam/postprocessor.py
@@ -147,7 +147,7 @@ def merge_current_origin_skims(all_skims_path, previous_skims_path, beam_output_
         logger.warning("Duplicated values in index: \n {0}".format(all_skims.loc[all_skims.duplicated()]))
         all_skims.drop_duplicates(inplace=True)
     all_skims.to_csv(all_skims_path, index=True)
-    cur_skims['totalWaitTimeInMinutes'] = cur_skims['waitTimeInMinutes'] * totals['completedRequests']
+    cur_skims['totalWaitTimeInMinutes'] = cur_skims['waitTimeInMinutes'] * cur_skims['completedRequests']
     totals = cur_skims.groupby(['timePeriod', 'reservationType']).sum()
     totals['matchedPercent'] = totals['completedRequests'] / totals['observations']
     totals['meanWaitTimeInMinutes'] = totals['totalWaitTimeInMinutes'] / totals['completedRequests']
