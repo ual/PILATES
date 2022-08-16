3fcda63404a1023d2d1e4207922d1d6c8e9d5a28
Zach Needell <zaneedell@lbl.gov>
2022-08-16 10:06:50 -0700

    upload new files

--- upload_to_gcloud.sh
+++ upload_to_gcloud.sh
@@ -10,6 +10,7 @@ if  [ $# -eq 2 ]
         gcloud alpha storage cp -r -n pilates/activitysim/output/year* gs://beam-core-outputs/$2/activitysim/
         gcloud alpha storage cp -r -n pilates/activitysim/output/pipeline.h5 gs://beam-core-outputs/$2/activitysim/pipeline.h5
         gcloud alpha storage cp -r -n pilates/activitysim/data/ gs://beam-core-outputs/$2/activitysim/data/
+        gcloud alpha storage cp -r -n pilates/postprocessing/output/ gs://beam-core-outputs/$2/inexus/
 
 else
     echo "Please provide a region (e.g. 'austin' or 'sfbay') and GCloud directory name"
