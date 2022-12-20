c62d9dc14294f11760d3ad07df4fe8b7c9dd235e
Cristian Poliziani <cpoliziani@lbl.gov>
2022-12-20 10:27:12 -0800

    update upload_to_s3

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -5,13 +5,13 @@ if  [ $# -eq 2 ]
         echo "Uploading results to S3 in Region: $1";
         echo "S3 directory: $2";
 
-        aws s3 sync pilates/beam/beam_output/$1/ s3://beam-outputs/pilates-outputs/$2/beam/ --region us-east-2 --exclude "*xml*";
+        aws s3 sync pilates/beam/beam_output/$1/ s3://beam-outputs/pilates-outputs/$2/beam/ --region us-east-2 --exclude "*xml*" --include "year-*" ;
         aws s3 sync pilates/activitysim/output/ s3://beam-outputs/pilates-outputs/$2/activitysim/ --region us-east-2 --exclude "*" --include "final*" --include "year*";
         aws s3 cp pilates/activitysim/output/pipeline.h5 s3://beam-outputs/pilates-outputs/$2/activitysim/pipeline.h5 --region us-east-2;
         aws s3 sync pilates/activitysim/data/ s3://beam-outputs/pilates-outputs/$2/activitysim/data/ --region us-east-2;
 #        aws s3 sync pilates/beam/beam_output/ s3://beam-outputs/pilates-outputs/"$2"/ --exclude "*" --include "*$1*" --region us-east-2;
-        aws s3 sync pilates/postprocessing/output/ s3://beam-outputs/pilates-outputs/$2/inexus/ --exclude ".git*"
-        aws s3 sync pilates/postprocessing/MEP/ s3://beam-outputs/pilates-outputs/$2/MEP/ --exclude ".git*"
+        aws s3 sync pilates/postprocessing/output/ s3://beam-outputs/pilates-outputs/$2/inexus/ --region us-east-2 --exclude ".git*"
+        aws s3 sync pilates/postprocessing/MEP/ s3://beam-outputs/pilates-outputs/$2/MEP/ --region us-east-2 --exclude ".git*"
 else
     echo "Please provide a region (e.g. 'austin' or 'sfbay') and S3 directory name"
 fi        
