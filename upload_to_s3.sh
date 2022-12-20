d0c610626e661ea56f545d05cc86c5e05fcc6aa1
Cristian Poliziani <cpoliziani@lbl.gov>
2022-12-20 10:03:19 -0800

    update upload_to_s3

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -5,8 +5,9 @@ if  [ $# -eq 2 ]
         echo "Uploading results to S3 in Region: $1";
         echo "S3 directory: $2";
 
-        aws s3 sync pilates/beam/beam_output/$1/ s3://beam-outputs/pilates-outputs/$2/beam/ --region us-east-2 --exclude "*xml*";
-        aws s3 sync pilates/activitysim/output/ s3://beam-outputs/pilates-outputs/$2/activitysim/ --region us-east-2 --exclude "*" --include "final*" --include "year*";
+        aws s3 sync pilates/beam/beam_output/$1/year-* s3://beam-outputs/pilates-outputs/$2/beam/ --region us-east-2 --exclude "*xml*";
+        aws s3 sync pilates/activitysim/output/final* s3://beam-outputs/pilates-outputs/$2/activitysim/ --region us-east-2 --exclude "*" --include "final*" --include "year*";
+        aws s3 sync pilates/activitysim/output/year* s3://beam-outputs/pilates-outputs/$2/activitysim/ --region us-east-2 --exclude "*" --include "final*" --include "year*";
         aws s3 cp pilates/activitysim/output/pipeline.h5 s3://beam-outputs/pilates-outputs/$2/activitysim/pipeline.h5 --region us-east-2;
         aws s3 sync pilates/activitysim/data/ s3://beam-outputs/pilates-outputs/$2/activitysim/data/ --region us-east-2;
 #        aws s3 sync pilates/beam/beam_output/ s3://beam-outputs/pilates-outputs/"$2"/ --exclude "*" --include "*$1*" --region us-east-2;
